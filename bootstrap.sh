f50f85f506e3d79187d4fa5ccbc0eb58c78cf042
Joseph Ilagan <joben.ilagan@gmail.com>
2014-06-01 09:43:56 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,12 @@ apt-get -y install mysql-server-5.5 php5-mysql libsqlite3-dev apache2 php5 php5-
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
-echo "Making mysql available remotely. Just remember how unsecure this is. NOT FOR ANYTHING OTHER THAN LOCAL DEVELOPMENT!"
+# Make MySQL available remotely. Warning: Not secure!! Use only for local development.
 	#comment mysql conf lines to allow remote connection
 	sed -i 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/my.cnf
 	sed -i 's/bind-address/#bind-address/g' /etc/mysql/my.cnf
 
-# allow mysql to be accessed remotely
+# Refresh MySQL to reflect changes.
 service mysql restart
 
 # Install Git
